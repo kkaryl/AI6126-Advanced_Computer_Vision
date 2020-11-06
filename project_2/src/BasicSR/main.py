47cd02a25ee07a2e281b58e7097e0e685dc5df04
karyl <jhong0819@gmail.com>
2020-11-06 08:50:59 +0800

    init commit for p2
    git clone basicSR
    git clone mmediting

---
+++ project_2/src/BasicSR/main.py
@@ -0,0 +1,24 @@
+from basicsr import train
+import argparse
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument('--opt', default='options/train/SRResNet_SRGAN/train_MSRResNet_x4.yml')
+    parser.add_argument('--launcher', default='none')
+    parser.add_argument('--local_rank', type=int, default=0)
+    return parser
+
+
+
+if __name__ == '__main__':
+    parser = parse_args()
+    args = parser.parse_args()
+    opt = train.parse_options(args=args)
+    train.main(opt)
+
+    # Transforms basicsr/data/transforms.py
+
+    # # Number of workers of reading data for each GPU
+    # num_worker_per_gpu: 6
+    # # Total training batch size
+    # batch_size_per_gpu: 16
